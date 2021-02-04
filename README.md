b9d72a6a9010de668b8172b6e63c3ebbf4f7b540
Cot Zhang <3063925756@qq.com>
2021-02-04 13:22:50 +0800

    Revert "!!X"
    This reverts commit c395aa2e654670f88295b9c4ac49804749538200.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ﻿# Cot 3D Modeler
 The most powered 3D builder for multi-platforms.
 ## Intro
-Cot 3D Modeler is the most powered 3D builder which is open-sourced and multi-platform supported. It's free for all and allowed for commercial use. It supports English and Chinese for full support. You can use it on Windows x86, x64 and ARM64. Also, Linux-based systems are supported for x86 and x64.
+Cot 3D Builder is the most powered 3D builder which is open-sourced and multi-platform supported. It's free for all and allowed for commercial use. It supports English and Chinese for full support. You can use it on Windows x86, x64 and ARM64. Also, Linux-based systems are supported for x86 and x64.
 ## Screenshots
 ![image](https://raw.fastgit.org/zjx2007/C3D_Cot-3D/master/Screenshot1.png)
 ## Credits
--- editor/window.js
+++ editor/window.js
@@ -18,15 +18,15 @@ _('closebtn').onclick = function() {
 _('maxbtn').onclick = function() {
     ipcRenderer.send('window-max');
 }
-_('panelistic-titletext').innerText = document.title;
+_('titletext').innerText = document.title;
 setInterval(function() {
-_('panelistic-titletext').innerText = document.title;
+    _('titletext').innerText = document.title;
 }, 800)
 ipcRenderer.on('maxmized', (event, arg) => {
     if (arg) {
-        _('maxbtn').src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAdCAYAAAA+YOU3AAAAmUlEQVRYR+2VQQqAIBREX7SJIOj+t6xN0aYwaFcmMYjGeIA/z+eoDRWupkJmDJ3r1GzapiMGXA/XI2M99hfbkjoqhrRAD6zABo8fVtiQIk8yZADmBMNFQXfAAozAdAN/wRYFHThjQIYOhiQX46+mr6pLJEmGJJqWfZhK6BiUKufMkA6TqUx49HNlyXJsWqbS9cil0qZt+ruBA49uGB6yMUNFAAAAAElFTkSuQmCC";
+        _('maxbtn').src = "img/window/reset.png";
     } else {
-        _('maxbtn').src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAdCAYAAAA+YOU3AAAAdElEQVRYR+2XwQ3AMAwCyf5Dt8oCgQeKavXyRpic+Xhp4FsDM4vQt7YGaUgfCFCPafV4wsCVzVZMJO3QzivRRH93gyITQgeYIG0g0emgRVaSUEw0dtAW/L7TCaUKpIpJkrapIXST5skL0pDmcrnVAUh/gPQLZcASHgcg2/4AAAAASUVORK5CYII=";
+        _('maxbtn').src = "img/window/max.png";
     }
     window.onresize = function() {
         ipcRenderer.send('window-get');
